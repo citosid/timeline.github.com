6b3eead767c4c72b5ae9553e8ffea21d952a335d
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2024-12-29 18:31:50 -0500

    feat: first working version

---
+++ assets/js/utils/dom.js
@@ -0,0 +1,29 @@
+const _DOM = {
+  create(tag, attributes = {}, children = []) {
+    const element = document.createElement(tag);
+    Object.entries(attributes).forEach(([key, value]) => {
+      if (key === 'className') {
+        element.className = value;
+      } else if (key.startsWith('on')) {
+        element.addEventListener(key.slice(2).toLowerCase(), value);
+      } else {
+        element.setAttribute(key, value);
+      }
+    });
+    children.forEach((child) => {
+      if (typeof child === 'string') {
+        element.appendChild(document.createTextNode(child));
+      } else {
+        element.appendChild(child);
+      }
+    });
+    return element;
+  },
+
+  createInputGroup(label, input) {
+    return _DOM.create('div', { className: 'input-group' }, [
+      _DOM.create('label', { className: 'input-label' }, [label]),
+      input,
+    ]);
+  },
+};
