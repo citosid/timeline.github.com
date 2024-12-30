3509dd64148353baa18d137290faa16336b1e883
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2024-12-30 00:53:27 -0500

    feat: improve ui and remove gated functionality

--- assets/js/modules/timeline.js
+++ assets/js/modules/timeline.js
@@ -8,6 +8,7 @@ class TimelineEvent {
     tags = [],
     link = '',
     background = { url: '', color: '#ffffff' },
+    bce = false,
   } = {}) {
     this.date = date;
     this.headline = headline;
@@ -17,13 +18,18 @@ class TimelineEvent {
     this.tags = tags;
     this.link = link;
     this.background = background;
+    this.bce = bce;
   }
 
   toJSON() {
     const date = this.date instanceof Date ? this.date : new Date(this.date);
+    let year = date.getFullYear().toString();
+    if (this.bce) {
+      year = -year;
+    }
     return {
       start_date: {
-        year: date.getFullYear().toString(),
+        year: year,
         month: (date.getMonth() + 1).toString().padStart(2, '0'),
         day: date.getDate().toString().padStart(2, '0'),
       },
@@ -36,13 +42,15 @@ class TimelineEvent {
       tags: this.tags,
       link: { url: this.link },
       background: this.background,
+      bce: this.bce,
     };
   }
 
   static fromJSON(json) {
-    const { start_date, text, media, group, tags, link, background } = json;
+    const { start_date, text, media, group, tags, link, background, bce } = json;
+    l(json);
     return new TimelineEvent({
-      date: new Date(`${start_date.year}-${start_date.month}-${start_date.day}`),
+      date: new Date(`${Math.abs(start_date.year)}-${start_date.month}-${start_date.day}`),
       headline: text.headline,
       description: text.text,
       media: media || {},
@@ -50,6 +58,7 @@ class TimelineEvent {
       tags: tags || [],
       link: link?.url || '',
       background: background || {},
+      bce: bce,
     });
   }
 }
